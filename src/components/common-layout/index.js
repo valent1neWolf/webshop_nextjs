c83347e0ec57d70c4ee95d6eb341a179a938614c
valent1neWolf <balifa02@gmail.com>
2024-07-09 10:37:50 +0200

    maintence

--- src/components/common-layout/index.js
+++ src/components/common-layout/index.js
@@ -3,5 +3,17 @@ import { auth } from "@/auth";
 
 export default async function CommonLayout({ children }) {
   const getSession = await auth();
-  return <ReduxProvider getSession={getSession}>{children}</ReduxProvider>;
+  const maintenance = true;
+  if (maintenance) {
+    return (
+      <div className="p-20 flex justify-center items-center">
+        <h2 className="text-3xl flex font-extrabold justify-center text-center">
+          We are currently under maintenance.
+          <br /> The site will be back soon...
+        </h2>
+      </div>
+    );
+  } else {
+    return <ReduxProvider getSession={getSession}>{children}</ReduxProvider>;
+  }
 }
